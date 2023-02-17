2569d38cfa89a080c6bd14f1d64d93305d066821
EffenrilAgung <effenrilagung300@gmail.com>
2023-02-18 01:35:28 +0700

    add cookies session

--- latihan-2/app.js
+++ latihan-2/app.js
@@ -1,11 +1,26 @@
 const path = require('path');
-
+const session = require('express-session');
+const mongodbSession = require('connect-mongodb-session');
 const express = require('express');
-
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mongodbSession(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore(
+  {
+    uri: 'mongodb://localhost:27017',
+    databaseName: 'auth-demo',
+    collection: 'session',
+  },
+  function (error) {
+    if (error) {
+      console.log(error);
+    }
+  }
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,11 +28,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+  session({
+    secret: 'super-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
+app.use(function (error, req, res, next) {
   res.render('500');
-})
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
--- latihan-2/routes/demo.js
+++ latihan-2/routes/demo.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const db = require('../data/database');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
@@ -16,14 +17,140 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
 
-router.post('/login', async function (req, res) {});
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData['confirm-email'];
+  const enteredPassword = userData.password;
+
+  const checkUser = db
+    .getDb()
+    .collection('users')
+    .findOne({ email: enteredEmail });
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredEmail !== enteredConfirmEmail ||
+    enteredPassword.trim().length < 6 ||
+    !enteredEmail.includes('@')
+  ) {
+    req.session.inputData = {
+      hasError: true,
+      message: 'Data tidak sesuai - ulangi kembali',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(() => {
+      res.redirect('/signup');
+    });
+    return;
+  }
+
+  if (checkUser) {
+    req.session.inputData = {
+      hasError: true,
+      message: 'email sudah terdaftar',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(() => {
+      res.redirect('/signup');
+    });
+    return;
+  }
+
+  // * bycrpt password
+  const hashedPassword = await bcrypt.genSalt(10).then((salt) => {
+    return bcrypt.hash(enteredPassword, salt);
+  });
+
+  console.log(hashedPassword);
+
+  const data = { email: enteredEmail, password: hashedPassword };
+
+  await db.getDb().collection('users').insertOne(data);
+
+  res.redirect('/login');
+});
+
+router.post('/login', async function (req, res) {
+  const formData = req.body;
+  const enteredEmail = formData.email;
+  const enteredPassword = formData.password;
+
+  console.log(req.session);
+
+  const users = await db
+    .getDb()
+    .collection('users')
+    .findOne({ email: enteredEmail });
+
+  if (!users) {
+    req.session.inputData = {
+      hasError: true,
+      message: 'Anda tidak dapat Login harap check kembali data',
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect('/login');
+    });
+    return;
+  }
+
+  //* compare password
+  const passwordAreEqual = await bcrypt.compare(
+    enteredPassword,
+    users.password
+  );
+
+  if (!passwordAreEqual) {
+    req.session.inputData = {
+      hasError: true,
+      message: 'Password Salah Cek Kembali',
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    req.session.save(() => {
+      res.redirect('/login');
+    });
+    return;
+  }
+
+  req.session.user = {
+    id: users._id,
+    email: users.email,
+  };
+  req.session.isAuthenticated = true;
+
+  req.session.save(() => {
+    res.redirect('/profile');
+  });
+  return;
+});
+
+router.get('/profile', async function (req, res) {
+  res.render('profile');
+});
 
 router.get('/admin', function (req, res) {
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.post('/logout', function (req, res) {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    req.session = null;
+    res.clearCookie('connect.sid');
+    res.redirect('/login');
+  });
+});
 
 module.exports = router;
